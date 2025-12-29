f746f53bc1880f572abae91e5d193b03501de90f
Max Guerreiro <max.well.guerreiro12@gmail.com>
2025-12-29 20:28:54 -0300

    Desafio nivel médio: Implementei uma lógica para receber entrada do úsuario sem permitir que ele posicione as peças fora do tabuleiro ou sobreponha dobre outra

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,13 +5,16 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+#define LINHAS 10
+#define COLUNAS 10
+
 int main() {
 
-    int matriz[10][10];
+    int matriz[LINHAS][COLUNAS];
 
     // esse loop setta o valor de cada posição em ZERO
-    for (int i=0; i<10; i++) {
-        for (int j=0; j<10; j++){
+    for (int i=0; i<LINHAS; i++) {
+        for (int j=0; j<COLUNAS; j++){
             matriz[i][j] = 0;
         }
     }
@@ -26,37 +29,40 @@ int main() {
     do { // Validação da entrada 
         scanf (" %3s", posicao);
 
-        coluna = toupper(posicao[0]) - 'A'; // Eu colocária "+1" para ser o valor da letra no alphabeto, mas eu preciso seja "-1" para a matriz
+        coluna = toupper(posicao[0]) - 'A'; // Eu colocária "+1" para ser o valor da letra no alphabeto, mas eu preciso que seja "-1" para a matriz
         // toupper para converter entrada para maiuscula caso nescessário
         linha = atoi(&posicao[1]) - 1; //-1 para ajustar para o indice da matriz
 
-        if (coluna > 10 || linha >10) {
+        if (coluna>=COLUNAS - 2 || linha>10) { // o programa não aceitará peças depois da coluna "i", se não a peça ficaria com parte fora do tabuleiro
             printf ("Posição inválida.. Tente outra posição\n");
-        } else {
+        } 
 
-            for (int i=0; i<3; i++) {
-
-                matriz[linha][coluna] = 3;
-
-                coluna ++;
-            }
+        int soma = coluna;
+        for (int i=0; i<3; i++) 
+        {
+            matriz[linha][soma] = 3;
+            soma ++; 
         }
         
-    } while (coluna > 10 || linha>10); 
+    } while (coluna >=COLUNAS - 2 || linha>10 ); 
 
-    int numeros = 1;
+    //Imprimindo na tela o tabuleiro com a peça posicionada
+
+    //Essas variaveis ajudaram a representar o tabuleiro com o numero das linhas e letras das colunas
+    int numeros = 1; 
     char letras = 'A';
     printf(" X  ");
-    for (int i = 0; i<10; i++) {
+    for (int i = 0; i<COLUNAS; i++) {
         printf ("%c  ", letras);
         letras ++;
     }
 
     printf ("\n");
 
-    for (int i=0; i<10; i++){
+
+    for (int i=0; i<LINHAS; i++){
         printf(" %d ", numeros);
-        for (int j=0; j<10; j++) {
+        for (int j=0; j<COLUNAS; j++) {
             printf (" %d ", matriz[i][j]);
         }
         printf("\n");
@@ -65,61 +71,157 @@ int main() {
 
     printf ("Escolha uma posição na vertical para posicionar seu segundo navil...\n");
 
-    char posicaoVertical[4];
+    // validando entrada para evtar peças fora do tabuleiro e sobreposições
+    do { 
+        scanf (" %3s", posicao); //A1
 
-    int numeroLinha;
-    int letraColuna;
+        coluna = toupper(posicao[0]) - 'A';
+        linha = atoi(&posicao[1])-1;
 
-    do {
-
-        scanf (" %3s", posicaoVertical); //A1
-
-        letraColuna = toupper(posicaoVertical[0]) - 'A';
-        numeroLinha = atoi(&posicaoVertical[1])-1;
-
-        if (letraColuna > 10 || numeroLinha > 10) {
-            printf ("Posição inválida.. Tente outra posição\n");
-        }
+        if (coluna > COLUNAS || linha >= COLUNAS - 2) {
+            printf ("Posição inválida.. Tente outra posição\n"); 
+        } 
  
-    } while (numeroLinha > 10 || letraColuna > 10);
+    } while (linha >= COLUNAS - 2 || coluna > COLUNAS);
 
     for (int i=0; i<3; i++) {
         
-        if (matriz[numeroLinha][letraColuna] == 3) {
+        if (matriz[linha][coluna] == 3) {
             printf("Você sobrepos uma embarcação, saindo do programa...");
             return 0;
         }
-        matriz[numeroLinha][letraColuna] = 3;
+        matriz[linha][coluna] = 3;
 
-        numeroLinha ++;
+        linha ++;
      }
 
     
     numeros = 1;
     letras = 'A';
     printf(" X  ");
-    for (int i=0; i<10; i++) {
+    for (int i=0; i<COLUNAS; i++) {
         printf("%c  ", letras);
         letras ++;
     }
     printf("\n");
     
-    for (int i=0; i<10; i++){
+    for (int i=0; i<LINHAS; i++){
         printf(" %d ", numeros);
-        for (int j=0; j<10; j++) {
+        for (int j=0; j<COLUNAS; j++) {
             printf (" %d ", matriz[i][j]);
         }
         printf("\n");
         numeros+=1;
     }
 
-    
-
-
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+    printf("Escolha duas posições para posicionar suas embarcações (Na diagonal)...\n");
+
+    printf("Digite a primeira posição...\n");
+
+    do { // validando entrada para evitar que o jogador escolha uma posição fora do tabuleiro
+        scanf(" %3s", posicao);
+
+        linha = atoi(&posicao[1]) - 1;
+        coluna = toupper(posicao[0]) -'A';
+
+        if (linha > LINHAS || coluna >= COLUNAS - 2) {
+            printf("Posição inválida, tente novamente...\n");
+        }
+    } while (linha > LINHAS || coluna >= COLUNAS - 2);
+
+    int somaLinha = linha;
+    int somaColuna = coluna;
+    for (int i=0; i<3; i++) { // validando se não exixte nenhum outro navil nessa posição
+        
+        if (matriz[somaLinha][somaColuna] == 3) {
+            printf("Já existe um navil nessa posição, encerrando programa...\n");
+            return 0;
+        }
+        somaColuna ++;
+        somaLinha ++;
+    }
+
+    for (int i=0; i<3; i++) {
+        matriz[linha][coluna] = 3;
+        linha ++;
+        coluna ++;
+    }
+
+    numeros = 1;
+    letras = 'A';
+    printf(" X  ");
+    for (int i=0; i<COLUNAS; i++) {
+        printf("%c  ", letras);
+        letras ++;
+    }
+    printf("\n");
+    
+    for (int i=0; i<LINHAS; i++){
+        printf(" %d ", numeros);
+        for (int j=0; j<COLUNAS; j++) {
+            printf (" %d ", matriz[i][j]);
+        }
+        printf("\n");
+        numeros+=1;
+    }
+
+
+    printf("Digite a segunda posição horizontal...\n");
+
+    do { // validando entrada para evitar que o jogador escolha uma posição fora do tabuleiro
+        scanf(" %3s", posicao);
+
+        linha = atoi(&posicao[1]) - 1;
+        coluna = toupper(posicao[0]) -'A';
+
+        if (linha > 10 || coluna > 10 || coluna < 2) {
+            printf("Posição inválida, tente novamente...\n");
+        }
+        
+    } while ((linha > LINHAS || coluna > COLUNAS) || coluna < 2);
+
+    somaColuna = coluna;
+    somaLinha = linha;
+    for (int i=0; i<3; i++) { // validando se não exixte nenhum outro navil nessa posição
+        
+        if (matriz[somaLinha][somaColuna] == 3) {
+            printf("Já existe um navil nessa posição, encerrando programa...\n");
+            return 0;
+        }
+        somaColuna --;
+        somaLinha ++;
+    }
+    
+    for (int i=0; i<3; i++) {
+        matriz[linha][coluna] = 3;
+        linha ++;
+        coluna --;
+    }
+
+    numeros = 1;
+    letras = 'A';
+    printf(" X  ");
+    for (int i=0; i<COLUNAS; i++) {
+        printf("%c  ", letras);
+        letras ++;
+    }
+    printf("\n");
+    
+    for (int i=0; i<LINHAS; i++){
+        printf(" %d ", numeros);
+        for (int j=0; j<COLUNAS; j++) {
+            printf (" %d ", matriz[i][j]);
+        }
+        printf("\n");
+        numeros+=1;
+    }
+
+
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
